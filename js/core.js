d124ae4892069f3b09a0841ecfa0228a255edea3
Danilo Alves <contatodanilovendas@gmail.com>
2026-08-01 18:03:24 -0300

    test: adicionar validação automática

---
+++ js/core.js
@@ -0,0 +1,18 @@
+(function (root, factory) {
+  const api = factory();
+
+  if (typeof module === "object" && module.exports) {
+    module.exports = api;
+  }
+
+  root.SuzyCore = api;
+})(typeof globalThis !== "undefined" ? globalThis : this, function () {
+  function localDateKey(date = new Date()) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  return { localDateKey };
+});
